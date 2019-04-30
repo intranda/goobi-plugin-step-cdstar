ca8de7f99f66c4e4b28f6df7ee41318e5f32e89b
Robert Sehr <robert.sehr@intranda.com>
2019-04-30 14:12:42 +0200

    changed plugin to step

---
+++ goobi-plugin-step-cdstar/src/main/java/org/goobi/api/mq/ticket/CDStarExportTicket.java
@@ -0,0 +1,188 @@
+package org.goobi.api.mq.ticket;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.goobi.api.mq.TaskTicket;
+import org.goobi.api.mq.TicketHandler;
+import org.goobi.beans.Process;
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginReturnValue;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import de.sub.goobi.export.dms.ExportDms;
+import de.sub.goobi.helper.Helper;
+import de.sub.goobi.helper.HelperSchritte;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.ExportFileException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.helper.exceptions.UghHelperException;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import lombok.extern.log4j.Log4j;
+import ugh.exceptions.DocStructHasNoTypeException;
+import ugh.exceptions.MetadataTypeNotAllowedException;
+import ugh.exceptions.PreferencesException;
+import ugh.exceptions.TypeNotAllowedForParentException;
+import ugh.exceptions.WriteException;
+
+@Log4j
+public class CDStarExportTicket extends ExportDms implements TicketHandler<PluginReturnValue> {
+
+    private static final Namespace metsNamespace = Namespace.getNamespace("mets", "http://www.loc.gov/METS/");
+    private static final Namespace xlink = Namespace.getNamespace("xlink", "http://www.w3.org/1999/xlink");
+
+    @Override
+    public PluginReturnValue call(TaskTicket ticket) {
+        log.info("got export ticket for " + ticket.getProcessName());
+
+        String userName = ticket.getProperties().get("userName");
+        String password = ticket.getProperties().get("password");
+        String archiveurl = ticket.getProperties().get("archiveurl");
+        Client client = ClientBuilder.newClient().register(new BasicAuthenticator(userName, password));
+
+        WebTarget archiveBase = client.target(archiveurl);
+        ArchiveInformation data = archiveBase.queryParam("with", "files").request(MediaType.APPLICATION_JSON).get(ArchiveInformation.class);
+
+        // export process
+        Process process = ProcessManager.getProcessById(ticket.getProcessId());
+
+        try {
+            exportWithImages = false;
+            startExport(process, process.getProjekt().getDmsImportImagesPath());
+        } catch (WriteException | PreferencesException | DocStructHasNoTypeException | MetadataTypeNotAllowedException
+                | TypeNotAllowedForParentException | IOException | InterruptedException | ExportFileException | UghHelperException | SwapException
+                | DAOException e) {
+            log.error(e);
+            return PluginReturnValue.ERROR;
+        }
+
+        Path metsFile = null;
+        if (process.getProjekt().isDmsImportCreateProcessFolder()) {
+            metsFile = Paths.get(process.getProjekt().getDmsImportImagesPath(), process.getTitel(), process.getTitel() + ".xml");
+        } else {
+            metsFile = Paths.get(process.getProjekt().getDmsImportImagesPath(), process.getTitel() + ".xml");
+        }
+
+        // read exported file and create/overwrite filegroup for cdstar
+
+        if (createAdditionalFileGroup(archiveurl, data, metsFile.toString())) {
+            // close step
+            Step stepToClose = null;
+
+            for (Step processStep : process.getSchritte()) {
+                if (processStep.getTitel().equals(ticket.getStepName())) {
+                    stepToClose = processStep;
+                    break;
+                }
+            }
+            if (stepToClose != null) {
+                new HelperSchritte().CloseStepObjectAutomatic(stepToClose);
+            }
+        }
+        //
+        //        System.out.println("***********************");
+        //        System.out.println(data.getProfile());
+        //        System.out.println(data.getState());
+        //        System.out.println(data.getCreated());
+        //        System.out.println(data.getModified());
+        //        System.out.println(data.getFile_count());
+        //        System.out.println(data.getOwner());
+        //
+        //        for (FileInformation info : data.getFiles()) {
+        //
+        //            System.out.println("*******");
+        //            System.out.println(archiveurl + "/" + info.getName());
+        //
+        //            System.out.println(info.getId());
+        //            System.out.println(info.getName());
+        //            System.out.println(info.getType());
+        //            System.out.println(info.getSize());
+        //            System.out.println(info.getCreated());
+        //            System.out.println(info.getModified());
+        //        }
+
+        return PluginReturnValue.FINISH;
+    }
+
+    private boolean createAdditionalFileGroup(String archiveurl, ArchiveInformation data, String metsFilename) {
+        SAXBuilder parser = new SAXBuilder();
+        Document metsDoc = null;
+        try {
+            metsDoc = parser.build(metsFilename);
+        } catch (JDOMException | IOException e) {
+            Helper.setFehlerMeldung("error while parsing mets file");
+            log.error("error while parsing mets file", e);
+            return false;
+        }
+        Element metsRoot = metsDoc.getRootElement();
+
+        Element fileSec = metsRoot.getChild("fileSec", metsNamespace);
+        List<Element> filegroups = fileSec.getChildren("fileGrp", metsNamespace);
+        for (Element filegroup : filegroups) {
+            if (filegroup.getAttributeValue("USE").equals("CDSTAR")) {
+                List<Element> metsFiles = filegroup.getChildren("file", metsNamespace);
+                for (int i = 0; i < metsFiles.size(); i++) {
+                    Element file = metsFiles.get(i);
+
+                    file.setAttribute("MIMETYPE", data.getFiles().get(i).getType());
+
+                    Element flocat = file.getChild("FLocat", metsNamespace);
+                    Attribute href = flocat.getAttribute("href", xlink);
+                    href.setValue(archiveurl + "/" + data.getFiles().get(i).getName());
+                }
+            }
+        }
+
+        List<Element> structMapList = metsRoot.getChildren("structMap", metsNamespace);
+
+        for (Element structMap : structMapList) {
+            if (structMap.getAttributeValue("TYPE").equals("PHYSICAL")) {
+                Element physSequence = structMap.getChild("div", metsNamespace);
+
+                List<Element> divs = physSequence.getChildren("div", metsNamespace);
+
+                for (int i = 0; i < divs.size(); i++) {
+                    Element div = divs.get(i);
+
+                    List<Element> fptrList = div.getChildren("fptr", metsNamespace);
+                    for (Element fptr : fptrList) {
+                        if (fptr.getAttributeValue("FILEID").contains("CDSTAR")) {
+                            fptr.setAttribute("CONTENTIDS", data.getFiles().get(i).getId());
+                        }
+                    }
+                }
+            }
+        }
+        XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
+        try {
+            FileOutputStream output = new FileOutputStream(metsFilename);
+            outputter.output(metsDoc, output);
+        } catch (IOException e) {
+            Helper.setFehlerMeldung("error while writing mets file");
+            log.error("error while writing mets file", e);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getTicketHandlerName() {
+        return "CDStarExport";
+    }
+
+}
---
+++ goobi-plugin-step-cdstar/src/main/java/org/goobi/api/mq/ticket/CDStarIngestTicket.java
@@ -0,0 +1,126 @@
+package org.goobi.api.mq.ticket;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import javax.jms.JMSException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.goobi.api.mq.TaskTicket;
+import org.goobi.api.mq.TicketGenerator;
+import org.goobi.api.mq.TicketHandler;
+import org.goobi.beans.Process;
+import org.goobi.beans.Processproperty;
+import org.goobi.production.enums.PluginReturnValue;
+
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.enums.PropertyType;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.persistence.managers.ProcessManager;
+import de.sub.goobi.persistence.managers.PropertyManager;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CDStarIngestTicket implements TicketHandler<PluginReturnValue> {
+
+    @Override
+    public PluginReturnValue call(TaskTicket ticket) {
+
+        log.info("got ingest ticket for " + ticket.getProcessName());
+
+        String userName = ticket.getProperties().get("userName");
+        String password = ticket.getProperties().get("password");
+        String url = ticket.getProperties().get("url");
+        String vault = ticket.getProperties().get("vault");
+
+        Integer processId = ticket.getProcessId();
+
+        Process process = ProcessManager.getProcessById(processId);
+
+        Client client = ClientBuilder.newClient().register(new BasicAuthenticator(userName, password));
+
+        WebTarget goobiBase = client.target(url);
+        WebTarget vaultBase = goobiBase.path(vault);
+
+        // create new archive
+        ArchiveInformation resp = vaultBase.request(MediaType.APPLICATION_JSON).post(null, ArchiveInformation.class);
+
+        log.info("created archive " + resp.getId());
+
+        // read archive metadata
+        WebTarget archiveBase = vaultBase.path(resp.getId());
+
+        // save archive id as property
+        Processproperty processproperty = new Processproperty();
+        processproperty.setProcessId(process.getId());
+        processproperty.setProzess(process);
+        processproperty.setTitel("archive-id");
+        processproperty.setType(PropertyType.general);
+        processproperty.setWert(resp.getId());
+        PropertyManager.saveProcessProperty(processproperty);
+
+        // upload images
+        List<Path> imageFiles = null;
+        try {
+            imageFiles = StorageProvider.getInstance().listFiles(process.getImagesOrigDirectory(false));
+        } catch (IOException | InterruptedException | SwapException | DAOException e1) {
+            log.error(e1);
+            return PluginReturnValue.ERROR;
+        }
+
+        WebTarget masterTarget = archiveBase.path("" + processId).path("master");
+
+        for (Path p : imageFiles) {
+            log.debug("upload file " + p.toString());
+            WebTarget imageTarget = masterTarget.path(p.getFileName().toString());
+
+            InputStream imageFile;
+            try {
+                imageFile = new FileInputStream(p.toFile());
+
+                String mimeType = Files.probeContentType(p);
+
+                imageTarget.request(MediaType.APPLICATION_JSON).put(Entity.entity(imageFile, mimeType), FileInformation.class);
+
+            } catch (IOException e) {
+                log.error(e);
+                return PluginReturnValue.ERROR;
+            }
+        }
+
+        TaskTicket exportTicket = TicketGenerator.generateSimpleTicket("CDStarExport");
+
+        exportTicket.setProcessId(ticket.getProcessId());
+        exportTicket.setProcessName(ticket.getProcessName());
+
+        exportTicket.setStepId(ticket.getStepId());
+        exportTicket.setStepName(ticket.getStepName());
+
+        exportTicket.setProperties(ticket.getProperties());
+        exportTicket.getProperties().put("archiveurl", archiveBase.getUri().toASCIIString());
+
+        try {
+            TicketGenerator.submitTicket(exportTicket, false);
+        } catch (JMSException e) {
+            log.error(e);
+            return PluginReturnValue.ERROR;
+        }
+
+        return PluginReturnValue.FINISH;
+    }
+
+    @Override
+    public String getTicketHandlerName() {
+        return "CDStarUpload";
+    }
+
+}
